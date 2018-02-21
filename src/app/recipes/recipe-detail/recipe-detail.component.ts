1284d2b8713ab0c7a32425f1d44f535e3c676440
ijemiD <deeolash@gmail.com>
2018-02-21 17:27:40 -0600

    3rd commit

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,8 +1,11 @@
+import { Ingredient } from './../../shared/ingredient.model';
+import { Store } from '@ngrx/store';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
+import * as ShoppingListActions from './../../shopping-list/store/shopping-list.actions';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -13,27 +16,26 @@ export class RecipeDetailComponent implements OnInit {
   recipe: Recipe;
   id: number;
 
-  constructor(private recipeService: RecipeService,
-              private route: ActivatedRoute,
-              private router: Router) {
-  }
+  constructor(
+    private recipeService: RecipeService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private store: Store<{ shoppingList: { ingredients: Ingredient[] } }>
+  ) {}
 
   ngOnInit() {
-    this.route.params
-      .subscribe(
-        (params: Params) => {
-          this.id = +params['id'];
-          this.recipe = this.recipeService.getRecipe(this.id);
-        }
-      );
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params['id'];
+      this.recipe = this.recipeService.getRecipe(this.id);
+    });
   }
 
   onAddToShoppingList() {
-    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+       this.store.dispatch(new ShoppingListActions.AddIngredients(this.recipe.ingredients));
   }
 
   onEditRecipe() {
-    this.router.navigate(['edit'], {relativeTo: this.route});
+    this.router.navigate(['edit'], { relativeTo: this.route });
     // this.router.navigate(['../', this.id, 'edit'], {relativeTo: this.route});
   }
 
@@ -41,5 +43,4 @@ export class RecipeDetailComponent implements OnInit {
     this.recipeService.deleteRecipe(this.id);
     this.router.navigate(['/recipes']);
   }
-
 }
--- src/app/shopping-list/store/shopping-list.reducers.ts
+++ src/app/shopping-list/store/shopping-list.reducers.ts
@@ -1,21 +1,43 @@
 import { Ingredient } from './../../shared/ingredient.model';
 import { Action } from '@ngrx/store'
-import *  as ShoppingListActions from './shopping-list-actions';
+import * as ShoppingListActions from './shopping-list.actions';
 
 const initialState = {
   ingredients : [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
   ]
-}
+};
 
-export function shoppingListReducer(state, action: ShoppingListActions.ShoppingListActions) {
+export function shoppingListReducer(state= initialState, action: ShoppingListActions.ShoppingListActions) {
    switch (action.type) {
      case ShoppingListActions.ADD_INGREDIENT :
        return {
          ...state,
          ingredients: [...state.ingredients, action.payload]
-       }
+       };
+
+     case ShoppingListActions.ADD_INGREDIENTS :
+        return {
+          ...state,
+          ingredients: [...state.ingredients, ...action.payload]
+        };
+
+     case ShoppingListActions.UPDATE_INGREDIENTS:
+        const ingredient = state.ingredients[action.payload.index];
+        const updIngredient = {
+          ...ingredient,
+          ...action.payload.ingredient
+        };
+        const ingredients = [...state.ingredients];
+        ingredients[action.payload.index] = updIngredient;
+        return { ...state, Ingredients: ingredients };
+
+      case ShoppingListActions.DELETE_INGREDIENT:
+          const oldIngredients = [...state.ingredients];
+          oldIngredients.splice(action.payload, 0);
+          return { ...state, Ingredients: oldIngredients };
+
        default:
          return state;
    }
